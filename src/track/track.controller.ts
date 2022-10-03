a0db899a1fb7785359e6525e1c9a25524ba48590
Oleksandr Dashko <leksdashko@gmail.com>
2022-10-03 22:40:11 +0200

    track services update

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { ObjectId } from 'mongoose';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { CreateCommentDto } from './dto/create-track.dto copy';
@@ -9,8 +19,23 @@ export class TrackController {
   constructor(private trackService: TrackService) {}
 
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(
+    @UploadedFiles()
+    files: {
+      picture?: Express.Multer.File[];
+      audio?: Express.Multer.File[];
+    },
+    @Body() dto: CreateTrackDto,
+  ) {
+    const { audio, picture } = files;
+
+    return this.trackService.create(dto, picture[0], audio[0]);
   }
 
   @Delete(':id')
