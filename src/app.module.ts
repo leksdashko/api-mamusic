aa578bc9ee458c0fc16b13a08f75b2f8bf1ffe09
Oleksandr Dashko <leksdashko@gmail.com>
2023-04-17 20:52:06 -0700

    updated main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,16 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import * as path from 'path';
-import { FileModule } from './file/file.module';
-import { TrackModule } from './track/track.module';
+import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import * as path from 'path'
+import {ServeStaticModule} from "@nestjs/serve-static";
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: path.resolve(__dirname, 'static'),
-    }),
-    MongooseModule.forRoot(
-      'mongodb+srv://leksdashko:221094@cluster0.grcnzoe.mongodb.net/?retryWrites=true&w=majority',
-    ),
-    TrackModule,
-    FileModule,
-  ],
+    imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.oeudk.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ]
 })
 export class AppModule {}
--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,71 +1,53 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Query,
-  UploadedFiles,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileFieldsInterceptor } from '@nestjs/platform-express';
-import { ObjectId } from 'mongoose';
-import { CreateTrackDto } from './dto/create-track.dto';
-import { CreateCommentDto } from './dto/create-track.dto copy';
-import { TrackService } from './track.service';
+import {Body, Controller, Delete, Get, Param, Post, Query, UploadedFiles, UseInterceptors} from "@nestjs/common";
+import {TrackService} from "./track.service";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {ObjectId} from "mongoose";
+import {CreateCommentDto} from "./dto/create-comment.dto";
+import {FileFieldsInterceptor} from "@nestjs/platform-express";
 
-@Controller('/track')
+
+@Controller('/tracks')
 export class TrackController {
-  constructor(private trackService: TrackService) {}
+    constructor(private trackService: TrackService) {    }
 
-  @Post()
-  @UseInterceptors(
-    FileFieldsInterceptor([
-      { name: 'picture', maxCount: 1 },
-      { name: 'audio', maxCount: 1 },
-    ]),
-  )
-  create(
-    @UploadedFiles()
-    files: {
-      picture?: Express.Multer.File[];
-      audio?: Express.Multer.File[];
-    },
-    @Body() dto: CreateTrackDto,
-  ) {
-    const { audio, picture } = files;
+    @Post()
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'picture', maxCount: 1 },
+        { name: 'audio', maxCount: 1 },
+    ]))
+    create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+        const {picture, audio} = files
+        return this.trackService.create(dto, picture[0], audio[0]);
+    }
 
-    return this.trackService.create(dto, picture[0], audio[0]);
-  }
+    @Get()
+    getAll(@Query('count') count: number,
+           @Query('offset') offset: number) {
+        return this.trackService.getAll(count, offset)
+    }
 
-  @Delete(':id')
-  delete(@Param('id') id: ObjectId) {
-    return this.trackService.delete(id);
-  }
+    @Get('/search')
+    search(@Query('query') query: string) {
+        return this.trackService.search(query)
+    }
 
-  @Get('/search')
-  search(@Query('query') query: string) {
-    return this.trackService.search(query);
-  }
+    @Get(':id')
+    getOne(@Param('id') id: ObjectId) {
+        return this.trackService.getOne(id);
+    }
 
-  @Get(':id')
-  getOne(@Param('id') id: ObjectId) {
-    return this.trackService.getOne(id);
-  }
+    @Delete(':id')
+    delete(@Param('id') id: ObjectId) {
+        return this.trackService.delete(id);
+    }
 
-  @Get()
-  getAll(@Query('count') count: number, @Query('offset') offset: number) {
-    return this.trackService.getAll(count, offset);
-  }
+    @Post('/comment')
+    addComment(@Body() dto: CreateCommentDto) {
+        return this.trackService.addComment(dto);
+    }
 
-  @Post('/comment')
-  addComment(@Body() dto: CreateCommentDto) {
-    return this.trackService.addComment(dto);
-  }
-
-  @Post('/listen/:id')
-  listen(@Param('id') id: ObjectId) {
-    return this.trackService.listen(id);
-  }
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId) {
+        return this.trackService.listen(id);
+    }
 }
